84240bd37cd0c4796be0d11aa1a1c12cdb5349e6
nahidrahat <nahidrahat007@gmail.com>
2024-08-17 20:44:28 +0600

    Web to App pro

---
+++ MeetPlanr/app/src/main/java/com/Insaaftech/Insaaf_tech/helper/MyHelper.java
@@ -0,0 +1,11 @@
+package com.Insaaftech.Insaaf_tech.helper;
+
+public interface MyHelper {
+    void loading();
+    void finishLoading();
+    void webGoBack();
+
+    void webLoadUrl(String url);
+    void errorLoading();
+
+} // myHelper End Here ===========
